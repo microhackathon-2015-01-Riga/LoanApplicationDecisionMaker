6e10f747bfa6330612f1a36d8e72a9dd71511d30
Sandris.Artemjevs <sandris.artemjevs@4finance.com>
2015-01-13 16:43:35 +0200

    retries

--- src/main/groovy/com/ofg/hackathon/DecisionMakerRun.groovy
+++ src/main/groovy/com/ofg/hackathon/DecisionMakerRun.groovy
@@ -1,11 +1,16 @@
 package com.ofg.hackathon
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet
 import com.ofg.infrastructure.discovery.EnableServiceDiscovery
 import com.ofg.infrastructure.environment.EnvironmentSetupVerifier
 import com.ofg.infrastructure.metrics.config.EnableMetrics
+import com.ofg.infrastructure.web.correlationid.EnableCorrelationId
 import com.ofg.infrastructure.web.resttemplate.fluent.EnableServiceRestClient
 import groovy.transform.TypeChecked
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.ServletRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 import static com.ofg.config.BasicProfiles.*
@@ -16,7 +21,13 @@ import static com.ofg.config.BasicProfiles.*
 @EnableServiceDiscovery
 @EnableMetrics
 @EnableServiceRestClient
+@EnableCorrelationId
 class DecisionMakerRun {
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/hystryx.stream");
+    }
 
     static void main(String[] args) {
         SpringApplication application = new SpringApplication(DecisionMakerRun)
--- src/main/groovy/com/ofg/hackathon/decisionmaker/notifying/AbstractNotifier.groovy
+++ src/main/groovy/com/ofg/hackathon/decisionmaker/notifying/AbstractNotifier.groovy
@@ -1,13 +1,18 @@
 package com.ofg.hackathon.decisionmaker.notifying
+
 import com.nurkiewicz.asyncretry.AsyncRetryExecutor
 import com.ofg.hackathon.decisionmaker.model.Decision
 import com.ofg.hackathon.decisionmaker.model.LoanApplication
+import com.ofg.infrastructure.hystrix.CorrelatedCommand
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
 import com.ofg.infrastructure.web.resttemplate.fluent.common.request.HttpMethod
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 
 import java.util.concurrent.ScheduledThreadPoolExecutor
+
+import static com.netflix.hystrix.HystrixCommand.Setter.withGroupKey
+import static com.netflix.hystrix.HystrixCommandGroupKey.Factory.asKey
 
 @Slf4j
 abstract class AbstractNotifier {
@@ -19,22 +24,29 @@ abstract class AbstractNotifier {
 
     void callService(String service, String relativeUrl, String method, Object body) {
         try {
-            HttpMethod httpMethod
 
-            def asyncRetryExecutor = new AsyncRetryExecutor(new ScheduledThreadPoolExecutor(1)).withMaxRetries(3).withMinDelay(1000)
-            def serviceBuilder = serviceRestClient.forService(service).retryUsing(asyncRetryExecutor)
-            if (method == 'put') {
-                httpMethod = serviceBuilder.put()
-            } else if (method == 'post') {
-                httpMethod = serviceBuilder.post()
-            }
+            new CorrelatedCommand<String>(withGroupKey(asKey("KEY"))) {
+                String doRun() {
+                    HttpMethod httpMethod
 
-            httpMethod.onUrl(relativeUrl).
-                    body(body).
-                    withHeaders().
-                    contentTypeJson().
-                    andExecuteFor().
-                    ignoringResponse()
+                    def asyncRetryExecutor = new AsyncRetryExecutor(new ScheduledThreadPoolExecutor(1)).withMaxRetries(3).withMinDelay(1000)
+                    def serviceBuilder = serviceRestClient.forService(service).retryUsing(asyncRetryExecutor)
+                    if (method == 'put') {
+                        httpMethod = serviceBuilder.put()
+                    } else if (method == 'post') {
+                        httpMethod = serviceBuilder.post()
+                    }
+
+                    httpMethod.onUrl(relativeUrl).
+                            body(body).
+                            withHeaders().
+                            contentTypeJson().
+                            andExecuteFor().
+                            ignoringResponse()
+                }
+            }.execute();
+
+
         } catch (Exception e) {
             log.warn('Exception caught: ', e)
         }
