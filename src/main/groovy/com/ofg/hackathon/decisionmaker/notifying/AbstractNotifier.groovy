8a2dd78db238706e84a793ececbf55e0a35c5871
Sandris.Artemjevs <sandris.artemjevs@4finance.com>
2015-01-13 14:45:19 +0200

    fix

--- src/main/groovy/com/ofg/hackathon/decisionmaker/notifying/AbstractNotifier.groovy
+++ src/main/groovy/com/ofg/hackathon/decisionmaker/notifying/AbstractNotifier.groovy
@@ -4,8 +4,10 @@ import com.ofg.hackathon.decisionmaker.model.Decision
 import com.ofg.hackathon.decisionmaker.model.LoanApplication
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
 import com.ofg.infrastructure.web.resttemplate.fluent.common.request.HttpMethod
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 
+@Slf4j
 abstract class AbstractNotifier {
 
     @Autowired
@@ -14,21 +16,24 @@ abstract class AbstractNotifier {
     abstract void notifyService(LoanApplication application, Decision decision);
 
     void callService(String service, String relativeUrl, String method, Object body) {
+        try {
+            HttpMethod httpMethod
 
-        HttpMethod httpMethod
+            if (method == 'put') {
+                httpMethod = serviceRestClient.forService(service).put()
+            } else if (method == 'post') {
+                httpMethod = serviceRestClient.forService(service).post()
+            }
 
-        if (method == 'put') {
-            httpMethod = serviceRestClient.forService(service).put()
-        } else if (method == 'post') {
-            httpMethod = serviceRestClient.forService(service).post()
+            httpMethod.onUrl(relativeUrl).
+                    body(body).
+                    withHeaders().
+                    contentTypeJson().
+                    andExecuteFor().
+                    anObject().
+                    ofType(String)
+        } catch (Exception e) {
+            log.warn('Exception caught: ', e)
         }
-
-        httpMethod.onUrl(relativeUrl).
-                body(body).
-                withHeaders().
-                contentTypeJson().
-                andExecuteFor().
-                anObject().
-                ofType(String)
     }
 }
